cddd23fbab52925bf9ac741bb90bf0730de3ad06
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-11-25 16:19:46 +0700

    Add project files.

---
+++ Exercise3_029/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_029
+{
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
